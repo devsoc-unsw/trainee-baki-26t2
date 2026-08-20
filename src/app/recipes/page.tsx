26462fdfe99729446e3e626796ff9294bd09c7c3
Ricky Guan <rickyguan1@gmail.com>
2026-08-20 21:52:56 +1000

    feat: setting up some stuff with routing

---
+++ src/app/recipes/page.tsx
@@ -0,0 +1,12 @@
+export default function RecipesPage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center gap-8 p-8">
+      <div className="text-center">
+        <h1 className="text-3xl font-semibold">Let Him Cook</h1>
+        <p className="mt-2 text-gray-500">
+          We're gonna have the recipe page here
+        </p>
+      </div>
+    </main>
+  )
+}
